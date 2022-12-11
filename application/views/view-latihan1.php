5bfb67ac3694d9c8c99f186a923ee5b7616d4813
giyusepp <giyuseppmail@gmail.com>
2022-12-11 09:27:05 +0700

    first commit

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <tittle>latihan 1</tittle>
+    </head>
+    <body>
+        halo kawan<br>
+        nilai 1=<?=$nilai1;?>
+        nilai 2=<?=$nilai2;?>
+        hasil<?=$nilai1. "+". $nilai2. "=". $hasil;?>
+    </body>
+</html>
